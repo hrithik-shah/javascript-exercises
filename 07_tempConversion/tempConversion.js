7deffb0759edebf6761cc37ce09dfd8b29774d21
Hrithik Shah <hrithik21.shah@gmail.com>
2024-09-24 20:02:27 -0400

    Complete 03-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  const temperatureCelsius = (temperatureFahrenheit - 32) * 5 / 9;
+  const roundedTemperatureCelsius = Math.round(temperatureCelsius * 10) / 10
+  return roundedTemperatureCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  const temperatureFahrenheit = temperatureCelsius * 9 / 5 + 32;
+  const roundedTemperatureFahrenheit = Math.round(temperatureFahrenheit * 10) / 10
+  return roundedTemperatureFahrenheit;
 };
 
 // Do not edit below this line
