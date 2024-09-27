2bf4b1bc8a7679766bf073f608715c4a89f605e1
Hrithik Shah <hrithik21.shah@gmail.com>
2024-09-27 08:09:03 -0400

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+	return values.reduce((acc, value) => acc + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+  return values.reduce((acc, value) => acc * value, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	return a === 0 ? 1 : a * factorial(a - 1);
 };
 
 // Do not edit below this line
